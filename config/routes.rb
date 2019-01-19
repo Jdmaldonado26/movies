9a9aafb8bb319703c8fd50b7ba7b77eaa80454b1
Raghu Betina <raghu@firstdraft.com>
2019-01-19 05:52:17 +0000

    Display associated collections on show pages

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
   # DELETE
   get("/delete_role/:id_to_remove", { :controller => "roles", :action => "destroy_row" })
+  get("/delete_role_from_actor/:id_to_remove", { :controller => "roles", :action => "destroy_row_from_actor" })
+  get("/delete_role_from_movie/:id_to_remove", { :controller => "roles", :action => "destroy_row_from_movie" })
 
   #------------------------------
 
@@ -74,6 +76,7 @@ Rails.application.routes.draw do
 
   # DELETE
   get("/delete_movie/:id_to_remove", { :controller => "movies", :action => "destroy_row" })
+  get("/delete_movie_from_director/:id_to_remove", { :controller => "movies", :action => "destroy_row_from_director" })
 
   #------------------------------
 
